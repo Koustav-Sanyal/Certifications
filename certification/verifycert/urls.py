7ff77bdd27c4233ac0a9d4527df73bc2156dae5c
Koustav Sanyal <sanyalkoustav2001@gmail.com>
2021-07-12 11:56:09 +0530

    Verification Process Added

---
+++ certification/verifycert/urls.py
@@ -0,0 +1,40 @@
+"""certification URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+app_name='verifycert'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    
+    path('CIT/', include('cit.urls')),
+    # path('RT/', include('RuntimeTerror.urls')),
+    # path('RC/', include('Recadathon.urls')),
+
+    path('cit/', include('cit.urls')),
+    # path('rt/', include('RuntimeTerror.urls')),
+    # path('rc/', include('Recadathon.urls')),
+    
+    path('<str:cert_id>', views.error, name='error'),
+    path('invalid/', views.invalid, name='invalid'),
+
+]
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
